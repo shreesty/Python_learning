7da9c8c41dd7b513901c2398be1af16dc1ac5f3a
its_shreesty <102721795+shreesty@users.noreply.github.com>
2022-12-13 17:15:06 +0545

    Add files via upload

---
+++ square_of_a_number.py
@@ -0,0 +1,15 @@
+//////////////
+'''
+
+Ask the user to enter a number. Print out the square of the number, but use the sep optional
+argument to print it out in a full sentence that ends in a period. Sample output is shown
+below
+
+'''
+'''
+Enter a number: 5
+print: The square of 5 is 25.
+'''
+
+number= int(input("Enter a number: "))
+print("The square of number is" ,number ** 2)
