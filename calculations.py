9a5f9845c85c86e2aee4d97171dcdc6567484822
its_shreesty <102721795+shreesty@users.noreply.github.com>
2022-12-13 17:15:34 +0545

    Add files via upload

---
+++ calculations.py
@@ -0,0 +1,16 @@
+# Write a program that computes and prints the result of (512 − 282)/ {(47 * 48)+5}
+
+a=512
+b=282
+c=47
+d=48
+e=5
+
+s= (a-b)
+#print(a)
+p=c * d
+print(p)#
+q= p + e
+#print(q)
+
+print(s / q)
