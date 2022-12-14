506cb74593f34fe1b9d2463fdd8b4988636b0cc1
its_shreesty <102721795+shreesty@users.noreply.github.com>
2022-12-14 17:00:04 +0545

    Add files via upload

---
+++ loop1.py
@@ -0,0 +1,9 @@
+'''
+
+Write a program that outputs 100 lines, numbered 1 to 100, each with your name on it. The
+output should look like the output below.
+
+'''
+name=input("Enter your name: ")
+for i in range(1,101):
+    print(i,name)
