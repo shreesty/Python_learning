1c45d917c4dccce3e5f4cb6876840be9b509c5fb
its_shreesty <102721795+shreesty@users.noreply.github.com>
2022-12-14 18:06:03 +0545

    Add files via upload

---
+++ kg_to_pounds.py
@@ -0,0 +1,10 @@
+
+
+'''
+Write a program that asks the user for a weight in kilograms and converts it to pounds. There
+are 2.2 pounds in a kilogram
+'''
+
+weight_in_kg= float(input("Enter the weight in km: "))
+pound= weight_in_kg * 2.2
+print(pound)
