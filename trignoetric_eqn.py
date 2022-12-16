9381b44d3aaa9e50fe33d640f1d882ea226b526b
its_shreesty <102721795+shreesty@users.noreply.github.com>
2022-12-16 15:10:23 +0545

    sin,cos,tan

---
+++ trignoetric_eqn.py
@@ -0,0 +1,11 @@
+
+
+#Write a program that asks the user for a number and then prints out the sine, cosine, and
+#tangent of that number.
+
+from math import sin, cos, tan
+num= int(input("Enter a number: "))
+print(sin(num))
+print(cos(num))
+print(tan(num))
+
