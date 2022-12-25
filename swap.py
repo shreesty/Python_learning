1d5255d362370e9a5502a1e30269c83a665fe230
its_shreesty <102721795+shreesty@users.noreply.github.com>
2022-12-25 14:33:50 +0545

    swapping value of x,y,z

---
+++ swap.py
@@ -0,0 +1,12 @@
+'''
+Write a program that swaps the values of three variables x, y, and z, so that x gets the value
+of y, y gets the value of z, and z gets the value of x.
+'''
+x=1
+y=2
+z=3
+
+x,y,z=y,z,x
+print("The value of x after swapping: ",x)
+print("The value of y after swapping: ",y)
+print("The value of z after swapping: ",z)
